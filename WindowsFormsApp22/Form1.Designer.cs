3e6380bc5606b1c9bc8d783d7e7151f9469c7bef
materikzentr@gmail.com <materikzentr@gmail.com>
2021-02-20 23:53:46 +0200

    обновление данных-2

--- WindowsFormsApp22/Form1.Designer.cs
+++ WindowsFormsApp22/Form1.Designer.cs
@@ -105,6 +105,64 @@ namespace WindowsFormsApp22
             this.HotDogCheckBox.Text = "Хот-дог";
             this.HotDogCheckBox.UseVisualStyleBackColor = true;
             // 
+
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.groupBox6);
+            this.groupBox1.Controls.Add(this.textBoxRadioCount);
+            this.groupBox1.Controls.Add(this.textBoxRadioSum);
+            this.groupBox1.Controls.Add(this.textBoxFuelPrice);
+            this.groupBox1.Controls.Add(this.comboBoxFuel);
+            this.groupBox1.Controls.Add(this.groupBoxFuel);
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 265);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Автозаправка";
+            // 
+
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Black;
+            this.label10.Location = new System.Drawing.Point(196, 161);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(27, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "грн.";
+            // 
+
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(196, 125);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "л.";
+            // 
+
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(6, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Цена";
+            // 
+
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(6, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Топливо";
+            // 
+
+
+
         }
 
         #endregion
